14d8e2d6e5bc91974a135e2bb4f8c4e076238b38
Avin Ben-Shemesh <ambrown111592@yahoo.com>
2022-09-10 01:11:23 -0400

    Added comments that help understande the unique features of the C++ fucntions.

--- Chapter1/sample.cpp
+++ Chapter1/sample.cpp
@@ -3,6 +3,11 @@
 // all functions must consist of 4 conponents
     // a return type (int), a function name (main)
     // parameter list, and function body {}.
+// the main function is require to have a return type of int
 int main() {
 
+    // returns a value back to the calling function
+    // the return value must be the same of that of the
+    // type of the calling function.
+    return 0; // type int (0)
 }
