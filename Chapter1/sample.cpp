5f69fbfc521382a14f7613a1f64e7e54e9ceada4
Avin Ben-Shemesh <ambrown111592@yahoo.com>
2022-09-10 01:04:49 -0400

    added test comments.

---
+++ Chapter1/sample.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+// all functions must consist of 4 conponents
+    // a return type (int), a function name (main)
+    // parameter list, and function body {}.
+int main() {
+
+}
