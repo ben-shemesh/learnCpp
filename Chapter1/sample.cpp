96c8596d13ec46354d2816004bd495c3b96f35cb
Avin Ben-Shemesh <ambrown111592@yahoo.com>
2022-09-10 01:46:37 -0400

    learned about istream and ostream of the standard library.

--- Chapter1/sample.cpp
+++ Chapter1/sample.cpp
@@ -1,3 +1,6 @@
+// represents input stream and ostream
+    // a stream is a sequence of characters read from 
+    // or written to an IO device i.e keyboard (istream) and terminal (ostream)
 #include <iostream>
 
 // all functions must consist of 4 conponents
@@ -5,6 +8,13 @@
     // parameter list, and function body {}.
 // the main function is require to have a return type of int
 int main() {
+    // to handle input we use an object of type istream (cin)
+        // for output we use an object of type ostream (cout)
+    std::cout << "Enter two numbers" << std::endl;
+    int v1 = 0, v2 = 0;
+    std::cin >> v1 >> v2;
+    std::cout << "The sum of:"<< v1 << "and" << v2 << " is: "
+              << v1 + v2 << std::endl;
 
     // returns a value back to the calling function
     // the return value must be the same of that of the
