1fe09826bc32c71d4e8ec6077d4f33023b8554d5
Avin Ben-Shemesh <ambrown111592@yahoo.com>
2022-09-11 04:36:18 -0400

    created a custom loop to better understand while and cin istream.

--- Chapter1/Unknown.cpp
+++ Chapter1/Unknown.cpp
@@ -8,6 +8,23 @@ int main(){
         sum += value;
 
     std::cout << "The Sum is " << sum << std::endl;
+/*
+    ex 1.16
+    create your own program that prints the sum of numbers
+    read from cin
+*/
+    int newSum = 0, newVal = 0;
+    while (std::cin >> newVal)
+    {
+        // if the newVal is greater is than 10 
+        // multiple it by 2 then procced with the while loop
+        if (newVal > 10) {
+            newVal *= 2;
+        }
+        newSum += newVal;
+    }
+        std::cout << "The sum is " << newSum << std::endl;
+    
     return 0;
 
 }
