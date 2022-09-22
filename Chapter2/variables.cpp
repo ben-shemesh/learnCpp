63a17644cbaf542bf0dfd1647fd5e1dc3b2dd55a
Avin Ben-Shemesh <ambrown111592@yahoo.com>
2022-09-22 01:31:07 -0400

    gained better understanding of inits inside and out of functions.

--- Chapter2/variables.cpp
+++ Chapter2/variables.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
-
+int money;
 int main () {
+std::cout << money << "\n";
     // must include the string library to use string that is not a
     // string literal
         // this is an intialization of the object sentence (initiliztion sets)
@@ -19,4 +20,16 @@ int main () {
     int c(pie), d = pie;
     double f = pie;
     std::cout << pie << "\n" << b << "\n" << c << "\n" << d << "\n";
+    std::cout << "---------\n";
+    // this v is not uninitalized its default value is and empty string
+        // this is defined by the string class of the std library
+    std::string empty;
+    std::cout << sentence.length() <<  "\n";
+
+    // will be truncated to 3
+    int input_value = 3.14;
+    std::cout << input_value << "\n";
+    double salary, wages = 9999.99;
+    std::cout << salary << "\n";
+    
 }
