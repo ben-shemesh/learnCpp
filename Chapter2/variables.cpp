be660d91385c8a4b26ec36efd638ffff8d920d20
Avin Ben-Shemesh <ambrown111592@yahoo.com>
2022-09-24 13:06:05 -0400

    added comments explaining scoping.

--- Chapter2/variables.cpp
+++ Chapter2/variables.cpp
@@ -2,7 +2,27 @@
 #include <string>
 int money;
 int main () {
-std::cout << money << "\n";
+    {   // inner scope1 start
+        // &
+        // outter scope of inner scope2
+        // declares a local variable money and initializes to 25;
+       int money = 25;
+       // prints 25
+      std::cout << money << "\n";
+      { // inner scope2 start inside of inner scope2
+      // has access to money variable in inner scope1
+      // defines it as 2, no longer 25
+        money = 2;
+        // prints 2
+      std::cout << money << "\n";
+
+      } // inner scope2 ends
+    } // inner scope1 ends
+    
+    // this will evaluate to 0 because the money variable in the global state
+    // is accessable* to all scoopes
+    // even though its declared but not defined its value will evaluate to 0
+    std::cout << money << "\n";
     // must include the string library to use string that is not a
     // string literal
         // this is an intialization of the object sentence (initiliztion sets)
@@ -10,6 +30,7 @@ std::cout << money << "\n";
     std::cout << sentence;
     // this is assignment of the object sentence (assignment changes)
     sentence = ("Cpp\n");
+    // sentence now has the value of "Cpp" instead of "hello"
     std::cout << sentence;
     
     // list initilizations
@@ -23,18 +44,18 @@ std::cout << money << "\n";
     std::cout << "---------\n";
     // this v is not uninitalized its default value is and empty string
         // this is defined by the string class of the std library
-    std::string empty;
-    std::cout << sentence.length() <<  "\n";
+//     std::string empty;
+//     std::cout << sentence.length() <<  "\n";
 
-    // will be truncated to 3
-    int input_value = 3.14;
-    std::cout << input_value << "\n";
-    double salary, wages = 9999.99;
-    std::cout << salary << "\n";
+//     // will be truncated to 3
+//     int input_value = 3.14;
+//     std::cout << input_value << "\n";
+//     double salary, wages = 9999.99;
+//     std::cout << salary << "\n";
 
-    int num = 23;
-    int result = num / 0;
-    // will evaluate to 0 division by 0 is infinity.
-    std::cout << "\n" << result << std::endl;
-    return 0;
-}
+//     int num = 23;
+//     int result = num / 0;
+//     // will evaluate to 0 division by 0 is infinity.
+//     std::cout << "\n" << result << std::endl;
+//     return 0;
+ }
