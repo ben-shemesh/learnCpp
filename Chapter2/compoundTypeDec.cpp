4740d0b0fdde3915b76af32315638eaac70fea4d
Avin Ben-Shemesh <ambrown111592@yahoo.com>
2022-10-01 08:57:17 -0400

    void pointers cannot dereference a type pointer.

--- Chapter2/compoundTypeDec.cpp
+++ Chapter2/compoundTypeDec.cpp
@@ -15,15 +15,17 @@ int main(){
             << pi << "\n"
             << "The address of the address of val is: "
             << ppi << "\n" << std::endl;
-            
+
    double obj = 3.14;
    std::cout << " the direct address of obj is: " << &obj <<std::endl;
    double *pip = &obj;
    std::cout << " the address of obj through pip is: " << pip <<std::endl;
-   void *pi2 = &obj;
+   double *pi2 = &obj;
    std::cout << " the address of obj through pi2 is: " << pi2 <<std::endl;
-   void **pii = &pi2;
-   std::cout << " the address of pi2 that hold th address of ob is: " << pii  << "\n" <<std::endl;
+   // you can only dereference the pointer twice if they are of the same type
+   // void pointers cannnot dereference a pointer of a specific type
+   double **pii = &pi2;
+   std::cout << " the value of obj is: " << **pii  << "\n" <<std::endl;
    pi2 = pip;
    bool itsTrue (pi2 == pip);
    std::cout << itsTrue << std::endl;
