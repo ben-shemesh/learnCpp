9709d92473268f1df39b6fc42875444fb7998e78
Avin Ben-Shemesh <ambrown111592@yahoo.com>
2022-10-08 00:01:50 -0400

    learning range for loops and cctypes.

--- Chapter3/stringExercises.cpp
+++ Chapter3/stringExercises.cpp
@@ -2,31 +2,31 @@
 #include <string>
 
 int main(){
-    // reads a line at a timne
-    std::string myInput;
-    getline(std::cin, myInput);
-    std::cout << myInput << "\n";
-    // reads a word at a time
-    std::string myWord;
-    std::cin >> myWord;
-    std::cout << myWord << "\n";
+    // // reads a line at a timne
+    // std::string myInput;
+    // getline(std::cin, myInput);
+    // std::cout << myInput << "\n";
+    // // reads a word at a time
+    // std::string myWord;
+    // std::cin >> myWord;
+    // std::cout << myWord << "\n";
 
-    // read two strings
-    std::string s1;
-    std::string s2;
-    getline(std::cin,s1);
-    getline(std::cin,s2);
-    // deteremine if they are equal
-    // if not report which of the two is larger
-    if (s1 != s2) 
-        if (s1 > s2)
-            std::cout << "s1 is larger than s2" <<"\n" 
-            << std::endl;
-        else
-            std::cout << "s2 is larger than s1" << "\n"
-            << std::endl;
-    else 
-        std::cout << "The strings are equal!";
+    // // read two strings
+    // std::string s1;
+    // std::string s2;
+    // getline(std::cin,s1);
+    // getline(std::cin,s2);
+    // // deteremine if they are equal
+    // // if not report which of the two is larger
+    // if (s1 != s2) 
+    //     if (s1 > s2)
+    //         std::cout << "s1 is larger than s2" <<"\n" 
+    //         << std::endl;
+    //     else
+    //         std::cout << "s2 is larger than s1" << "\n"
+    //         << std::endl;
+    // else 
+    //     std::cout << "The strings are equal!";
 
     std::cout << "enter two strings\n";
     std::string s3;
